34cf877345c8fe268a5f455167ff9017425c5042
Specht <otto@specht.it>
2025-03-31 22:06:38 +0200

    Make logger cooler

--- pkg/logger/log.go
+++ pkg/logger/log.go
@@ -15,21 +15,36 @@ const (
 
 func (l LogLevel) String() string {
 	switch l {
+	case DEBUG:
+		return "DBG"
 	case INFO:
 		return "INF"
 	case WARN:
 		return "WRN"
 	case ERROR:
 		return "ERR"
-	case DEBUG:
-		return "DBG"
 	default:
 		return "UKN"
 	}
 }
 
+func color(l LogLevel) string {
+	switch l {
+	case DEBUG:
+		return "\033[37m"
+	case INFO:
+		return "\033[36m"
+	case WARN:
+		return "\033[33m"
+	case ERROR:
+		return "\033[31m"
+	default:
+		return ""
+	}
+}
+
 func log(level LogLevel, msg string) {
-	fmt.Printf("[%s] %s\n", level.String(), msg)
+	fmt.Printf("%s[%s] %s\033[0m\n", color(level), level.String(), msg)
 }
 
 func Debug(msg string) {
